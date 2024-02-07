eb66ea03eb65b6cdc4e547a21673c8ac775c50fa
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-07 10:55:42 -0300

    feat: add function validate

--- src/routes/ClientRoute.js
+++ src/routes/ClientRoute.js
@@ -4,8 +4,11 @@ const ClientRoute = require("express").Router()
 //Controller
 const {registerCleint} = require("../controller/ClientController")
 
+//Middleware
+const validate = require("../middlewares/handleValidation")
 
-ClientRoute.post("/register/client", registerCleint)
+
+ClientRoute.post("/register/client", validate, registerCleint)
 
 
 module.exports = ClientRoute
