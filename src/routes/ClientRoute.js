21be5a5fe88aa2301a5f0fd3af90123c793cada7
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-07 14:43:30 -0300

    feat: added new route update client

--- src/routes/ClientRoute.js
+++ src/routes/ClientRoute.js
@@ -2,16 +2,17 @@ const ClientRoute = require("express").Router()
 
 
 //Controller
-const {registerClient, getOneClient, getAllClients} = require("../controller/ClientController")
+const {registerClient, getOneClient, getAllClients, editClient} = require("../controller/ClientController")
 
 //Middleware
 const validate = require("../middlewares/handleValidation")
-const {clientRegisterValidation} = require("../middlewares/clientValidation")
+const {clientValidation} = require("../middlewares/clientValidation")
 
 
-ClientRoute.post("/register/client", clientRegisterValidation(), validate, registerClient)
+ClientRoute.post("/register/client", clientValidation(), validate, registerClient)
 ClientRoute.get("/one/client/:id", validate, getOneClient)
 ClientRoute.get("/all/clients", validate, getAllClients)
+ClientRoute.put("/edit/client/:id", clientValidation(), validate, editClient)
 
 
 module.exports = ClientRoute
