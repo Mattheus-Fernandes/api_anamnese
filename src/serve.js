42e64d3b18aa847f00f86663861f54dd2abf88e4
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-07 10:11:47 -0300

    feat: create serve

---
+++ src/serve.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const port = 8000
+const cors = require("cors")
+
+
+//Config JSON and form data response
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+//Solver CORS
+app.use(cors())
+
+
+//Connection DB
+require("./config/index").connect()
+
+
+//Test route
+app.get("/", (req, res) => {
+  res.send("API Working")
+})
+
+
+app.listen(port, () => console.log(`The serve is running on the port ${port}`))
