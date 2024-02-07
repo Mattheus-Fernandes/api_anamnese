e2588867c80cb1b3777b99b723e59343f05dcab4
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-07 13:55:46 -0300

    feat: create new route get one client

--- src/routes/ClientRoute.js
+++ src/routes/ClientRoute.js
@@ -2,7 +2,7 @@ const ClientRoute = require("express").Router()
 
 
 //Controller
-const {registerClient} = require("../controller/ClientController")
+const {registerClient, getOneClient} = require("../controller/ClientController")
 
 //Middleware
 const validate = require("../middlewares/handleValidation")
@@ -10,6 +10,7 @@ const {clientRegisterValidation} = require("../middlewares/clientValidation")
 
 
 ClientRoute.post("/register/client", clientRegisterValidation(), validate, registerClient)
+ClientRoute.get("/one/client/:id", validate, getOneClient)
 
 
 module.exports = ClientRoute
